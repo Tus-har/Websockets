a4065038c0416a83fba47f0b989677ddd091e96e
Tushar <tusharv.ec.16@nsit.net.in>
2019-06-22 23:42:41 +0530

    Changed port

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import {NestExpressApplication} from "@nestjs/platform-express";
 import {join} from "path";
 
 async function bootstrap() {
+    const port = process.env.PORT || 5000 ;
     const app = await NestFactory.create<NestExpressApplication>(ChatModule);
     app.useStaticAssets(join(__dirname, '..', 'public'));
-    await app.listen(5000);
+    await app.listen(port);
 }
 bootstrap();
